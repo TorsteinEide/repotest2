2cc87cf79b6136ebd134643301353f7b178a8ada
Torstein <taurstein@gmail.com>
2021-02-25 13:08:53 +0100

    added lines

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,13 +1,23 @@
 public class Person {
+
+    /**
+     *
+     */
     private String name;
 
-    public Person(String name)
+
+    public Person()
     {
-        this.name = name;
+        this.name = "mr worldwide";
     }
 
     public String getName()
     {
         return this.name;
+    }
+
+    public void getQuote()
+    {
+        System.out.println(getName());
     }
 }
